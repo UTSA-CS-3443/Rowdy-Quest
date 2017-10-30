29cb8ac6724217e5d099f569507e36e3af798949
Brandon Black <bblack118@gmail.com>
2017-10-30 16:28:34 -0600

    test

---
+++ RowdyQuest/src/model/tile/local/Sidewalk.java
@@ -0,0 +1,23 @@
+package model.tile.local;
+
+import model.texture.Texture;
+import model.tile.Tile;
+
+/**
+ * 
+ * @author Brandon Black
+ *
+ *         This class defines the sidewalk Tile
+ */
+public class Sidewalk extends Tile {
+
+	/**
+	 * Constructor
+	 * 
+	 * @param id
+	 */
+	public Sidewalk(int id) {
+		super(Texture.sidewalk, id);
+		// TODO Auto-generated constructor stub
+	}
+}
