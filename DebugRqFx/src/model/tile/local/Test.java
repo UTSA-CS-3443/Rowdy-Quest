e97fe8d04014d999385c7481d6b3785919c71182
Tyler-Clarkson <clarkson3226@gmail.com>
2017-11-12 00:28:24 -0600

    test commit for noclassdeferror

---
+++ DebugRqFx/src/model/tile/local/Test.java
@@ -0,0 +1,14 @@
+package model.tile.local;
+
+import javafx.scene.image.Image;
+import model.texture.Texture;
+import model.tile.Tile;
+
+public class Test extends Tile{
+
+	public Test(int id) {
+		super(Texture.black, id, null);
+		// TODO Auto-generated constructor stub
+	}
+
+}
