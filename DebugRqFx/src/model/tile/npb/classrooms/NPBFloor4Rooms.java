fc6580be1682e756b435d1619a54ddbe3a493595
Marlene Garza <marlenagarza@yahoo.com>
2017-11-20 12:34:30 -0600

    NBP floors 4 and 5

---
+++ DebugRqFx/src/model/tile/npb/classrooms/NPBFloor4Rooms.java
@@ -0,0 +1,96 @@
+package model.tile.npb.classrooms;
+
+import java.util.ArrayList;
+
+import model.tile.Tile;
+
+/**
+ * 
+ * @author Marlene Garza
+ *
+ */
+
+public class NPBFloor4Rooms {
+	public static ArrayList<Tile> npb4List = new ArrayList<>();
+
+	/**
+	 * 1st column starting from bottom on the right side of the hallway
+	 */	
+	
+	public static Tile npb4c0 = new DoorTopTile(185,"NPB 4.S1");
+	public static Tile npb4c1 = new DoorTopTile(186,"NPB 4.120-3");
+	public static Tile npb4c2 = new DoorTopTile(187,"NPB 4.108");
+	public static Tile npb4c3 = new DoorTopTile(188,"NPB 4.106");
+	public static Tile npb4c4 = new DoorTopTile(189,"NPB 4.102");
+	public static Tile npb4c5 = new DoorTopTile(190,"NPB 4.102A17-1");
+	public static Tile npb4c6 = new DoorTopTile(191,"NPB 4.146");
+	public static Tile npb4c7 = new DoorTopTile(192,"NPB 4.102-1");
+	public static Tile npb4c8 = new DoorTopTile(193,"NPB 4.E3");
+	
+	/**
+	 *  right column starting on the bottom 
+	 */
+	
+	public static Tile npb4c9 = new DoorRightTile(194,"NPB 4.120");
+	public static Tile npb4c10 = new DoorRightTile(195,"NPB 4.120V-1");
+	public static Tile npb4c11 = new DoorRightTile(196,"NPB 4.120");
+	
+	
+	/**
+	 * Far row starting at left towards the top 
+	 */
+	
+	public static Tile npb4c12 = new DoorBottomTile(197,"NPB 4.112");
+	public static Tile npb4c13 = new DoorBottomTile(198,"NPB 4.110");
+	
+
+
+/**
+ * Bottom row starting at left
+ */
+	
+	public static Tile npb4c14 = new DoorLeftTile(199,"NPB 4.152");
+	public static Tile npb4c15 = new DoorLeftTile(200,"NPB 4.150");
+	public static Tile npb4c16 = new DoorLeftTile(201,"NPB 4.4.148");
+
+	/**
+	 * Top row starting at left
+	 */
+	
+	public static Tile npb4c17 = new DoorRightTile(202,"NPB 4.154");
+	public static Tile npb4c18 = new DoorRightTile(203,"NPB 4.156");
+	
+	public NPBFloor4Rooms(){
+
+	npb4List.add(npb4c0);
+	npb4List.add(npb4c1);
+	npb4List.add(npb4c4);
+	npb4List.add(npb4c3);
+	npb4List.add(npb4c4);
+	npb4List.add(npb4c5);
+	npb4List.add(npb4c6);
+	npb4List.add(npb4c7);
+	npb4List.add(npb4c8);
+
+	
+	npb4List.add(npb4c9);
+	npb4List.add(npb4c10);
+	npb4List.add(npb4c11);
+	
+	npb4List.add(npb4c12);
+	npb4List.add(npb4c13);
+	
+	npb4List.add(npb4c14);
+	npb4List.add(npb4c15);
+	npb4List.add(npb4c16);
+	
+	npb4List.add(npb4c17);
+	npb4List.add(npb4c18);
+	
+
+
+	
+	}
+}
+
+
---
+++ DebugRqFx/src/model/tile/npb/classrooms/NPBFloor5Rooms.java
@@ -0,0 +1,103 @@
+package model.tile.npb.classrooms;
+
+import java.util.ArrayList;
+
+import controller.events.LocalEvents;
+import model.tile.Tile;
+
+/**
+ * @author Marlene Garza
+ * 
+ *
+ */
+
+public class NPBFloor5Rooms {
+	
+	public static ArrayList<Tile> npb5List = new ArrayList<>();
+	/**
+	 * Far Left column of doors starting from bottom 
+	 */
+	
+	public static Tile npb5c0 = new DoorTopTile(204,"NPB 5.S1");
+	public static Tile npb5c1 = new DoorTopTile(205,"NPB 5.110");
+	public static Tile npb5c2 = new DoorTopTile(206,"NPB 5.108");
+	public static Tile npb5c3 = new DoorTopTile(207,"NPB 5.106");
+	public static Tile npb5c4 = new DoorTopTile(208,"NPB 5.102");
+	public static Tile npb5c5 = new DoorTopTile(209,"NPB 5.126-1");
+	public static Tile npb5c6 = new DoorTopTile(210,"NPB 5.140A");
+	public static Tile npb5c7 = new DoorTopTile(211,"NPB 5.140");
+	public static Tile npb5c8 = new DoorTopTile(212,"NPB 5.146");
+	public static Tile npb5c9 = new DoorTopTile(213,"NPB 5.140-1");
+	public static Tile npb5c10 = new DoorTopTile(214,"NPB 5.180-A");
+	public static Tile npb5c11 = new DoorTopTile(215,"NPB 5.S2");
+	
+	
+	/**
+	 * 2nd column starting from bottom 
+	 */
+	public static Tile npb5c12 = new DoorBottomTile(216,"NPB 5.102-2");
+	public static Tile npb5c13 = new DoorBottomTile(217,"NPB 5.122");
+	public static Tile npb5c14 = new DoorBottomTile(218,"NPB 5.130");
+	
+	
+	/**
+	 * Top row starting at left
+	 */
+	public static Tile npb5c15 = new DoorRightTile(219,"NPB 5.180");
+	
+	/**
+	 * First row starting at bottom starting at left
+	 */
+	
+	public static Tile npb5c16 = new DoorLeftTile(220,"NPB 5.102-1");
+	
+	
+	/**
+	 * Top row towards the top starting at left
+	 */
+	public static Tile npb5c17 = new DoorRightTile(221,"NPB 5.156");
+	public static Tile npb5c18 = new DoorRightTile(222,"NPB 5.152");
+	
+	/**
+	 * Bottom row towards the top starting at left
+	 */
+	public static Tile npb5c19 = new DoorLeftTile(223,"NPB 5.154");
+	public static Tile npb5c20 = new DoorLeftTile(224,"NPB 5.150");
+	public static Tile npb5c21 = new DoorLeftTile(225,"NPB 5.148");
+
+	
+	
+	public NPBFloor5Rooms(){
+		npb5List.add(npb5c0);
+		npb5List.add(npb5c1);
+		npb5List.add(npb5c2);
+		npb5List.add(npb5c3);
+		npb5List.add(npb5c4);
+		npb5List.add(npb5c5);
+		npb5List.add(npb5c6);
+		npb5List.add(npb5c7);
+		npb5List.add(npb5c8);
+		npb5List.add(npb5c9);
+		npb5List.add(npb5c10);
+		npb5List.add(npb5c11);
+		npb5List.add(npb5c12);
+		npb5List.add(npb5c13);
+		npb5List.add(npb5c14);
+		npb5List.add(npb5c15);
+		npb5List.add(npb5c16);
+		npb5List.add(npb5c17);
+		npb5List.add(npb5c18);
+		npb5List.add(npb5c19);
+		npb5List.add(npb5c20);
+		npb5List.add(npb5c21);
+
+
+
+		LocalEvents.NPBList.add(NPBFloor5Rooms.npb5List);
+
+		}
+
+
+	
+	
+}
